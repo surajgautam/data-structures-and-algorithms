14d3365f28cb66c15d17959b129ce4473ea3373b
surajgautam <teamwarrior200@gmail.com>
2021-06-15 22:21:42 +0545

    feat: add test and inplace rotate matrix implementation

--- src/main/java/com/surajgautam/datastructures/and/algorithms/arraysandstrings/RotateMatrix.java
+++ src/main/java/com/surajgautam/datastructures/and/algorithms/arraysandstrings/RotateMatrix.java
@@ -15,15 +15,34 @@ public class RotateMatrix {
         return rotatedMatrix;
     }
 
+    public int[][] rotateInPlaceSolution(int[][] matrix) {
+        int length = matrix.length; //nxn matrix
+        /*
+        transpose a matrix and then swap the elements from the rows using 2 pointer solution
+        for eg:
+         [1 2 3        [1   4   7     [7    4   1
+          4 5 6   =>    2   5   8  => 8     5   2
+          7 8 9[        3   6   9]    9     6   3]
+         */
+        //first transpose
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
 
-//    private void printMatrix(int[][] rotatedMatrix) {
-//        for (int i = 0; i < rotatedMatrix.length; i++) {
-//            for (int j = 0; j < rotatedMatrix.length; j++) {
-//                System.out.print(rotatedMatrix[i][j]);
-//                System.out.print('\t');
-//            }
-//            System.out.println();
-//        }
-//    }
+        //reverse columns
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < (length / 2); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length - 1 - j];
+                matrix[i][length - 1 - j] = temp;
+            }
+        }
 
+        return matrix;
+
+    }
 }
