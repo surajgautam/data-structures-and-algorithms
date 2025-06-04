976a84dc6bacf75c0778c0fe432dd36e7241071a
Suraj.Gautam <teamwarrior200@gmail.com>
2025-06-03 20:55:16 -0500

    fix: update java and remove lombok

--- src/main/java/com/surajgautam/datastructures/and/algorithms/linkedlist/Node.java
+++ src/main/java/com/surajgautam/datastructures/and/algorithms/linkedlist/Node.java
@@ -1,17 +1,36 @@
 package com.surajgautam.datastructures.and.algorithms.linkedlist;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
 public class Node<T> {
 
     private T value;
     private Node next;
 
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
